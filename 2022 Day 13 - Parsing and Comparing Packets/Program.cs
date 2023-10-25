e6680f61a285ed637a6b6f024fba9706feea42f7
t.nugent <t.nugent@hotmail.co.uk>
2023-10-25 16:37:47 +0100

    Add project files.

---
+++ 2022 Day 13 - Parsing and Comparing Packets/Program.cs
@@ -0,0 +1,20 @@
+﻿
+class Program
+{
+
+
+
+
+    public static void Main(string[] args)
+    {
+        string pathTest = @"C:\Users\Tom\Documents\Advent\2022 Day 13 - Parsing and Comparing Packets\2022 Day 13 - Parsing and Comparing Packets\data_test.txt";
+        string pathFull = @"C:\Users\Tom\Documents\Advent\2022 Day 13 - Parsing and Comparing Packets\2022 Day 13 - Parsing and Comparing Packets\data_full.txt";
+
+        string[] instructions = System.IO.File.ReadAllLines(pathTest);
+
+        foreach(string instruction in instructions)
+        {
+            Console.WriteLine(instruction);
+        }
+    }
+}
